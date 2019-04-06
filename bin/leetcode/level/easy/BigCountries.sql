9153659332405ceeea2a622e30b9314155f121e8
yunyoung1819 <yunyoung1819@hanmail.net>
2019-04-06 12:57:35 +0900

    Leetcode 595 Problem Solving

--- bin/leetcode/level/easy/BigCountries.sql
+++ bin/leetcode/level/easy/BigCountries.sql
@@ -35,3 +35,12 @@ SELECT name, population, area
 FROM World
 WHERE area > 3000000 OR population > 25000000;
 
+
+/** Solvig different **/
+SELECT name, population, area
+FROM World
+WHERE area > 3000000
+UNION
+SELECT name, population, area
+FROM World
+WHERE population > 25000000;
