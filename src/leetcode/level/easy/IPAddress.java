718e4fd508bde56563b620f0aa61825684271bf2
yunyoung1819 <yunyoung1819@hanmail.net>
2019-12-28 11:50:28 +0900

    leetcode 1108 Problem Solving

--- src/leetcode/level/easy/IPAddress.java
+++ src/leetcode/level/easy/IPAddress.java
@@ -1,5 +1,19 @@
 package leetcode.level.easy;
 
+/**
+ * problem : 1108
+ * title : Defanging an IP Address 
+ * link : https://leetcode.com/problems/defanging-an-ip-address/
+ * date : 2019.12.28
+ * author : YUN YOUNG 
+ * 
+ * description : Given a valid (IPv4) IP address, return a defanged version of that IP address.
+ * A defanged IP address replaces every period "." with "[.]".  
+ *
+ */
 public class IPAddress {
-
+	public String defangIPaddr(String address) {
+		String newAddr = address.replace(".", "[.]");
+		return newAddr;
+	}
 }
