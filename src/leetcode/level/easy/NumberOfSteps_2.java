5194ed4853c61fd336a330e36326485c55365548
yunyoung1819 <yunyoung1819@hanmail.net>
2020-04-05 23:34:09 +0900

    Leetcode #NumberOfStpes solution add_2020.04.05_yunyoung

--- src/leetcode/level/easy/NumberOfSteps_2.java
+++ src/leetcode/level/easy/NumberOfSteps_2.java
@@ -8,23 +8,14 @@ package leetcode.level.easy;
  * 작 성 일 : 2020.04.05
  * 작 성 자 : YUN YOUNG
  * 문제내용 : Given a non-negative integer num, return the number of steps to reduce it to zero.
- * If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
+ * if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1
  *
  */
-public class NumberOfSteps {
-	public int numberOfSteps (int num) {
-		int count = 1;
-		while (true) {
-			if (num % 2 == 0) {
-				num /= 2;
-			} else {
-				num -= 1;
-			}
-			if (num == 0) {
-				break;
-			}
-			count++;
+public class NumberOfSteps_2 {
+	public int numberOfSteps (int n) {
+		if (n == 0) {
+			return 0;
 		}
-		return count;
+		return 1 + numberOfSteps(n % 2 == 0 ? n / 2 : n - 1);
 	}
 }
