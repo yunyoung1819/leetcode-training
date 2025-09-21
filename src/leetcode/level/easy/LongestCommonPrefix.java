c8a242e6ba4e9d993a9fef0ee88c5caaa49b2561
yunyoung1819 <yunyoung1819@hanmail.net>
2025-09-21 18:44:18 +0900

    feat: solving problem

---
+++ src/leetcode/level/easy/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+package leetcode.level.easy;
+
+/**
+ * title: Longest Common Prefix
+ * date: 2025.09.21
+ * author: yun young
+ * link: https://leetcode.com/problems/longest-common-prefix/description/?envType=problem-list-v2&envId=array
+ * description: Write
+ *
+ */
+public class LongestCommonPrefix {
+
+}
